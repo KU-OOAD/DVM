a167a3c9e88ad65f53d04f6b9aa63562822c893b
Sang-Min Kang <rdrniel12345@gmail.com>
2024-06-09 23:25:58 +0900

    test update

--- src/test/java/manager/VerificationManagerTest.java
+++ src/test/java/manager/VerificationManagerTest.java
@@ -1,12 +1,32 @@
 package manager;
 
+import data.VerificationCode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class VerificationManagerTest {
 
+    private VerificationManager manager = new VerificationManager();
+
     @Test
     void getVerificationCode() {
+        String code = manager.getVerificationCode();
+        System.out.println(code);
+        assertEquals(code.getClass(), String.class);
+        assertFalse(code.isBlank());
+    }
 
+    @Test
+    void saveCodeAndVerifyTest() {
+        String strCode = manager.getVerificationCode();
+        VerificationCode code = new VerificationCode(strCode, "01", 1);
+        manager.saveCode(code);
+        VerificationCode code2 = manager.verifyCode(strCode);
+        assertSame(code.getCode(), code2.getCode());
+        assertEquals(code.getDrinkType(), code2.getDrinkType());
+        assertSame(code.getDrinkNum(), code2.getDrinkNum());
     }
 
 }
